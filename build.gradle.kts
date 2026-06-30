3aa6266c0551aac2fd8d4d495e6ebffb69b710db
ravily96-coder <ravily96@gmail.com>
2026-06-30 15:19:26 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,4 @@
 plugins {
   id("org.jetbrains.kotlin.android") version "1.9.0" apply false
   id("com.android.application") version "8.2.0" apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
-}
+  
