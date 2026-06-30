8e29bedea0285660c28df74dd371796f9c5de8f1
ravily96-coder <ravily96@gmail.com>
2026-06-30 15:11:41 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-tasks.register('wrapper', Wrapper) {
-    gradleVersion = '8.5'
-}
 plugins {
   id("org.jetbrains.kotlin.android") version "1.9.0" apply false
   id("com.android.application") version "8.2.0" apply false
